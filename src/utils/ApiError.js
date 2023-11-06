a08a5d925598faf7ded0a0971b88288f0856af6d
DevanshSK <devanshs843@gmail.com>
2023-11-07 00:55:44 +0530

    Created a ApiError wrapper class

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,1 +1,26 @@
 // Create a custom error Class for error handling
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Something went wrong",
+    errors = [],
+    stack = ""
+  ) {
+    // Call the parent constructor
+    super(message);
+    this.statusCode = statusCode;
+    // TODO: Research about this.data field in nodejs
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export { ApiError };
